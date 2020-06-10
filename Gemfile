a5b6c6d1412a9606542d2903e474ebbc9b7a52bc
EC2 Default User <ec2-user@ip-172-31-32-14.ap-northeast-1.compute.internal>
2020-06-10 17:25:24 +0000

    プロフィール写真機能のためにDB更新

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'devise'
 # FontAwesome
 gem 'font-awesome-sass'
 
+# CarrierWave
+gem 'carrierwave'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_143049) do
+ActiveRecord::Schema.define(version: 2020_06_10_063107) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_143049) do
     t.string "user_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
