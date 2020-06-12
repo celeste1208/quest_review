8631f22d0275eba593ccfe1c474d6c175d4cf8c8
EC2 Default User <ec2-user@ip-172-31-32-14.ap-northeast-1.compute.internal>
2020-06-12 08:19:52 +0000

    設定ページに自分以外の人がアクセス出来ないように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_owner
   before_action :set_user, only: [:show, :update]
 
   def show
@@ -9,6 +10,10 @@ class UsersController < ApplicationController
   end
 
   private
+  
+    def authenticate_owner
+      redirect_to root_path if current_user.id != params[:id].to_i
+    end
   
     def set_user
       @user = User.find(params[:id]).decorate
