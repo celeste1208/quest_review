3e03de5f0c73e76626db4624bb994d53e381296f
EC2 Default User <ec2-user@ip-172-31-32-14.ap-northeast-1.compute.internal>
2020-06-14 11:57:25 +0000

    クエスト詳細ページでタブ切り替えした場合、非表示中のコメントにカーソルが合ってしまう不具合を修正

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,31 +4,23 @@
 jQuery(document).on('click', '.review_content_button', function() {
   $('.review_content_button').css({'color':'#949faf'});
   $(this).css({'color':'black'});
+  $('.comment_container').children().css('visibility','hidden');
   switch ($("div:eq(0)",this).text()) {
     // テキストで取得するのは変更に弱すぎるのでやめる
     case "良かった点":
       $('.review_header_bar').animate({'left':'0%'}, {duration: 300});
       $('.comment_container').css({'transform':'translate(0%,0px)'});
-      $('.sticky_test a div:eq(0)').css({'background-color':'#2E88FB'});
-      $('.sticky_test a div:eq(1)').text('「良かった点」を投稿');
-      var query = $('.sticky_test a').attr('href').replace(/opinion_type=.*&/, 'opinion_type=positive&');
-      $('.sticky_test a').attr('href', query);
+      $('.comment:eq(0)').css('visibility','visible');
       break;
     case "改善点":
       $('.review_header_bar').animate({'left':'33.3%'}, {duration: 300});
       $('.comment_container').css({'transform':'translate(-100%,0px)'});
-      $('.sticky_test a div:eq(0)').css({'background-color':'#EB6566'});
-      $('.sticky_test a div:eq(1)').text('「改善点」を投稿');
-      var query = $('.sticky_test a').attr('href').replace(/opinion_type=.*&/, 'opinion_type=negative&');
-      $('.sticky_test a').attr('href', query);
+      $('.comment:eq(1)').css('visibility','visible');
       break;
     case "質問・その他":
       $('.review_header_bar').animate({'left':'66.6%'}, {duration: 300});
       $('.comment_container').css({'transform':'translate(-200%,0px)'});
-      $('.sticky_test a div:eq(0)').css({'background-color':'#F3A533'});
-      $('.sticky_test a div:eq(1)').text('「質問・その他」を投稿');
-      var query = $('.sticky_test a').attr('href').replace(/opinion_type=.*&/, 'opinion_type=question&');
-      $('.sticky_test a').attr('href', query);
+      $('.comment:eq(2)').css('visibility','visible');
       break;
   }
 });
