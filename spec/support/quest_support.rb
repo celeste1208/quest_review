fd9b50d786646e8b1aa344fde2f8f1aa6370a3f0
celeste1208 <takashi.okamoto.1990@gmail.com>
2020-06-29 02:52:38 +0900

    rspecエラーを修正

--- spec/support/quest_support.rb
+++ spec/support/quest_support.rb
@@ -1,24 +1,22 @@
 module QuestSupport
   def create_quest_with(develop_id, responsible, title, content)
     visit new_quest_path
-    input_form(develop_id, responsible, title, content)
+    input_quest_form(develop_id, responsible, title, content)
     click_button '登録する'
   end
 
   def update_quest_with(id, develop_id, responsible, title, content)
     visit edit_quest_path(id)
-    input_form(develop_id, responsible, title, content)
+    input_quest_form(develop_id, responsible, title, content)
     click_button '更新する'
   end
 
-  def input_form(develop_id, responsible, title, content)
+  def input_quest_form(develop_id, responsible, title, content)
     fill_in 'quest_develop_id', with: develop_id
     fill_in 'quest_responsible', with: responsible
     fill_in 'quest_title', with: title
     fill_in 'quest_content', with: content
   end
-
-  private_class_method :input_form
 end
 
 RSpec.configure do |config|
