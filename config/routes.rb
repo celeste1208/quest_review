b1286de19e8d468bdfe43f66466bfbc2b34ec99a
celeste1208 <takashi.okamoto.1990@gmail.com>
2020-07-05 03:19:34 +0900

    不要なrouteを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  
+
   devise_for :users
   devise_scope :user do
     get '/users', to: 'devise/registrations#new'
   end
-  
-  resources :users, only: [:show, :update, :update_icon]
+
+  resources :users, only: [:show, :update]
   resources :quests
   resources :comments, only: [:create, :update, :destroy]
 end
